7aab22a8f02a626638bb1fd8da4845d027a9d0f0
machearn <machearn.n@gmail.com>
2023-12-20 22:01:21 -0500

    disable neovide transparent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,5 +3,4 @@
 -- Add any additional options here
 vim.opt.guifont = "UbuntuMono Nerd Font Mono:h19"
 vim.opt.clipboard:append({ "unnamedplus" })
-vim.g.neovide_transparency = 0.8
 vim.g.neovide_input_macos_alt_is_meta = true
