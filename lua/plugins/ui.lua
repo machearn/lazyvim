aae4e7075ba957f1ce199aee2fc4bb5ff6a7fabe
machearn <machearn.n@gmail.com>
2023-12-20 22:01:43 -0500

    disable `noisy` noice

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -33,4 +33,17 @@ return {
       opts.config.header = vim.split(logo, "\n")
     end,
   },
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+      opts.presets.lsp_doc_border = true
+    end,
+  },
 }
