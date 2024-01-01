689254735e8b3876ff5745522395d3b2a17f3202
machearn <machearn.n@gmail.com>
2024-01-01 03:22:09 -0500

    add `markdown` and `python` module

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+
+    { import = "lazyvim.plugins.extras.lang.python" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
