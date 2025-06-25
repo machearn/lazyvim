d6b45d0775bfb6c92d023f7517d87939d6c0a730
machearn <machearn.n@gmail.com>
2025-06-25 17:39:58 -0400

    change gui setting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.mouse = ""
-vim.opt.guifont = "UbuntuMono Nerd Font Mono:h19"
 vim.opt.clipboard:append({ "unnamedplus" })
-vim.g.neovide_input_macos_alt_is_meta = true
+if vim.g.neovide then
+  vim.opt.guifont = "Hack Nerd Font Mono:h18"
+  vim.g.neovide_input_macos_option_key_is_meta = "both"
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_cursor_vfx_mode = { "torpedo", "sonicboom" }
+end
